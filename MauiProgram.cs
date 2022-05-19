16b3a93880b984e41173cb41d6641ac129086262
lumiasnow <tianwang123@outlook.com>
2022-05-19 20:13:55 +0800

    init

---
+++ MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using MauiApp2.Data;
+
+namespace MauiApp2;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+        builder.Services.AddSingleton<IMediaPicker>(MediaPicker.Default);
+        builder.Services.AddSingleton<IFileSystem>(FileSystem.Current);
+
+        return builder.Build();
+    }
+}
